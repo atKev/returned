9590f55cd5630a01a8c766959a8449e139edd1d8
atKev <K.Brueggemann28@gmx.de>
2021-09-01 10:03:18 +0200

    Rooms and Warps
    Added Warp to the doors. Char can enter every room now

--- Returned/Assets/Creator Kit - RPG/Scripts/Gameplay/CameraController.cs
+++ Returned/Assets/Creator Kit - RPG/Scripts/Gameplay/CameraController.cs
@@ -11,27 +11,19 @@ namespace RPGM.Gameplay
     /// </summary>
     public class CameraController : MonoBehaviour
     {
-        public Transform target;
-        public float smoothing;
-        public Vector2 maxPosition;
-        public Vector2 minPosition;
+        public Transform focus;
+        public float smoothTime = 2;
 
-        void Start()
+        Vector3 offset;
+
+        void Awake()
         {
-            
+            offset = focus.position - transform.position;
         }
 
-        void LateUpdate()
+        void Update()
         {
-            if(transform.position != target.position)
-            {
-                Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-
-                targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-                targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-
-                transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-            }
+            transform.position = Vector3.Lerp(transform.position, focus.position - offset, Time.deltaTime * smoothTime);
         }
     }
 }
---
+++ Returned/Assets/Creator Kit - RPG/Scripts/Gameplay/Warp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    public Transform warpTarget;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("An object Collided.");
+        other.gameObject.transform.position = warpTarget.position;
+        Camera.main.transform.position = warpTarget.position;
+    }
+}
